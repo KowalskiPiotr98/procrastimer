06ca955dc1dcc5eedddeca12bdfa6675b19ec070
Piotr Kowalski <piko98@proton.me>
2024-06-23 12:19:50 +0200

    Implement db insertable interface on `Game`

--- api/games/model.go
+++ api/games/model.go
@@ -2,12 +2,19 @@ package games
 
 import (
 	"github.com/gofrs/uuid"
+	"procrastimer/common/repository"
 )
 
 type Game struct {
 	Id      uuid.UUID
 	Name    string
 	AddedOn int64
+}
+
+var _ repository.CreatableWithId = (*Game)(nil)
+
+func (g *Game) SetId(id uuid.UUID) {
+	g.Id = id
 }
 
 func NewGame(name string) *Game {
